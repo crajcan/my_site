ab48641ee44664928cc9c12d1d673a848c360505
Carson Rajcan <carajcan@gmail.com>
2019-01-03 22:24:22 -0600

    Write snapshot test with jest and enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+it('Renders my name', () => {
+  const app = shallow(<App />);
+  expect(app).toMatchSnapshot();
 });
